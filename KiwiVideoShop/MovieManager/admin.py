3af40b1af3ba885ee85a122757be80b887cf83b4
SunshinePrime <39469749+SunshinePrime@users.noreply.github.com>
2018-10-01 17:40:47 +1300

    Sprint 2/Alfred
    Authentication added.

--- KiwiVideoShop/MovieManager/admin.py
+++ KiwiVideoShop/MovieManager/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from MovieManager.models import Director, Movie, MovieInstance, Genre
 
-# Register your models here.
+
+# Define the admin class
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# Register the admin class with the associated model
+admin.site.register(Director, DirectorAdmin)
+
+
+class MovieInstanceInline(admin.TabularInline):
+    model = MovieInstance
+
+
+# Register the Admin classes for Movie using the decorator
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'director', 'display_genre', 'year')
+    inlines = [MovieInstanceInline]
+
+
+# Register the Admin classes for MovieInstance using the decorator
+@admin.register(MovieInstance) 
+class MovieInstanceAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'status', 'renter', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    
+    fieldsets = (
+        (None, {
+            'fields': ('movie','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back','renter')
+        }),
+    )
+
+
+admin.site.register(Genre)
--- KiwiVideoShop/MovieManager/urls.py
+++ KiwiVideoShop/MovieManager/urls.py
@@ -3,5 +3,13 @@ from MovieManager import views
 
 
 urlpatterns = [
+	path('', views.index, name='index'),
+	path('movies/', views.MovieListView.as_view(), name='movies'),
+	path('movie/<int:pk>', views.MovieDetailView.as_view(), name='movie-detail'),
+	path('directors/', views.DirectorListView.as_view(), name='directors'),
+	path('director/<int:pk>', views.DirectorDetailView.as_view(), name='director-detail'),
+]
 
+urlpatterns += [   
+    path('mymovies/', views.RentedMoviesByUserListView.as_view(), name='my-rented'),
 ]
